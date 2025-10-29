7a8fc96ed9a5449a297f1fa046b5f2dfacd1f655
marijoe-cloud <marsanjuan@gbox.adnu.edu.ph>
2025-10-29 23:23:29 +0800

    Linked Routing for signup & regstration

--- frontend/BookAFaci/src/App.jsx
+++ frontend/BookAFaci/src/App.jsx
@@ -15,31 +15,3 @@ const App = () => {
 }
 
 export default App
-
-/*
-import { useState } from "react";
-import UserTypeSelection from "./components/UserTypeSelection";
-import InternalSignUp from "./components/InternalSignUp";
-import ExternalSignUp from "./components/ExternalSignUp";
-import "./App.css";
-
-function App() {
-  const [step, setStep] = useState(1);
-  const [userType, setUserType] = useState("");
-
-  const handleContinue = (type) => {
-    setUserType(type);
-    setStep(2);
-  };
-
-  return (
-    <div className="app">
-      {step === 1 && <UserTypeSelection onContinue={handleContinue} />}
-      {step === 2 && userType === "internal" && <InternalSignUp />}
-      {step === 2 && userType === "external" && <ExternalSignUp />}
-    </div>
-  );
-}
-
-export default App;
-*/
