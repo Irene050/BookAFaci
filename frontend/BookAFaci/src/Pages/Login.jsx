f1a316257dfcd3340698257aa7238ad52405f891
Jeremiah <jeremiah.lirag@gmail.com>
2025-11-21 16:35:04 +0800

    Login form layout

--- frontend/BookAFaci/src/Pages/Login.jsx
+++ frontend/BookAFaci/src/Pages/Login.jsx
@@ -5,13 +5,39 @@ import loginbg from '../assets/Gradient blur.png'
 
 const Login = () => {
   return (
-    <div className="min-h-screen bg-cover bg-center flex flex-col items-center justify-center"
-      style={{ backgroundImage: `url(${loginbg})` }}>
-      <div className="w-full text-center mb-3">
-        <h1 className="text-[19px] font-bold text-white">BOOKAFACI</h1>
+    <div className="min-h-screen relative transition-all">
+      <div className="absolute inset-0 flex">
+        <div className="w-[60%] bg-[url('/src/assets/left.jpeg')] bg-cover bg-center
+        min-[320px]:w-[0%]
+        min-[375px]:w-[0%]
+        min-[425px]:w-[0%]
+        sm:w-[0%]
+        md:w-[0%]
+        lg:w-[60%]
+        xl:w-[70%]
+        " style={{
+        }}>
+          <h1 className="mt-[25%] text-center text-[10rem] font-bold text-white">BOOKAFACI</h1>
+        </div>
+
+        <div className="w-[40%] bg-[url('/src/assets/right.png')] bg-cover bg-center
+        min-[320px]:w-[100%]
+        min-[375px]:w-[100%]
+        min-[425px]:w-[100%]
+        sm:w-[100%]
+        md:w-[100%]
+        lg:w-[40%]
+        xl:w-[40%]
+        ">
+            <div className="w-full mb-3 mt-[25%]">
+              <div className='flex flex-col items-center justify-center mt-10'>
+                <LoginForm />
+              </div>
+            </div>
+            <title>BookAFaci Login</title>
+        </div>
       </div>
-      <title>BookAFaci Login</title>
-      <LoginForm />
+
     </div>
   )
 }
