03b82e568abeecabf6b7eca88762bf4725079b1a
Jeremiah <jeremiah.lirag@gmail.com>
2025-12-03 19:23:20 +0800

    CHANGES: Faci modal button behavior and landing nav format and sidebar hover styles

--- frontend/BookAFaci/src/Pages/landing.jsx
+++ frontend/BookAFaci/src/Pages/landing.jsx
@@ -6,6 +6,7 @@ import Landingnav from '../components/landingnav';
 function landing() {
     return (
         <div className="flex min-h-screen transition-all">
+            <title>BookAFaci</title>
             <main className="flex-1 bg-center bg-cover min-h-screen relative pb-5 
                   min-[320px]:w-[350px] max-[640px]:w-[450px] md:w-[450px] lg:w-[450px]"
                 style={{
@@ -17,13 +18,14 @@ function landing() {
                 <Landingnav />
                 <div className='ml-[10rem] flex h-screen v-screen items-center text-white font-bold text-5xl gap-5'>
                     <div className='font-inter text-[3rem] flex grow group relative justify-normal'>
-                        <article className="text-wrap">
-                            <h3>BOOKAFACI</h3>
-                            <p className='text-[2rem] font-normal'>Description & tagline</p>
-                            <Link to="/user-select" className="mt-4 inline-block bg-white/20 hover:bg-white/30 text-white font-normal text-lg py-2 px-4 rounded">
-                                Get Started
-                            </Link>
-                        </article>
+                        <div className='text-wrap'>
+                            <article className='mb-3'>
+                                <h3>BOOKAFACI</h3>
+                                <p className='text-[2rem] font-normal'>Description & tagline</p>
+                            </article>
+
+                            <a className=' font-inter text-white bg-transparent border p-2 rounded-full text-[1.5rem] hover:bg-white hover:text-black mix-blend-screen transition-all' href="/user-select">Get Started</a>
+                        </div>
                     </div>
                     <div className='font-inter flex grow group relative justify-center'>LOGO</div>
                 </div>
@@ -31,5 +33,4 @@ function landing() {
         </div>
     )
 }
-
 export default landing
--- frontend/BookAFaci/src/components/sidebar.jsx
+++ frontend/BookAFaci/src/components/sidebar.jsx
@@ -138,9 +138,14 @@ export function SidebarItem({ icon, text, active, alert, onClick}) {
         <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? '' : 'top-2'}`} />
       )}
 
+      {!active && !expanded && (
+        <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-[#63bbff] text-white text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>
+        {text}
+        </div>
+      )}
       {!expanded && (
-        <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-[#63bbff] text-white-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>
-          {text}
+        <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-[#63bbff] text-white text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>
+        {text}
         </div>
       )}
       </button>
