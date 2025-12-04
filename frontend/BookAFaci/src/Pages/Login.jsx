8c2606f7348811ba51b92afad17b90b49958f303
Jest <jeremiah.lirag@gmail.com>
2025-12-04 20:51:38 +0800

    Login Mobile Fix

--- frontend/BookAFaci/src/Pages/Login.jsx
+++ frontend/BookAFaci/src/Pages/Login.jsx
@@ -17,15 +17,15 @@ const Login = () => {
         xl:w-[70%]
         " style={{
         }}>
-          <h1 className="text-center text-[10rem] font-bold text-white
+          <a href="/" className='text-center text-[10rem] font-bold text-white
         min-[320px]:text-[0rem]
         min-[375px]:text-[0rem]
         min-[425px]:text-[0rem]
+        min-[1366px]:text-[10rem]
         sm:text-[0rem]
         md:text-[0rem]
         lg:text-[6rem]
-        xl:text-[10rem]
-          ">BOOKAFACI</h1>
+        xl:text-[7rem]'>BOOKAFACI</a>
         </div>
 
         <div className="w-[40%] bg-[url('/src/assets/right.png')] bg-cover bg-center
